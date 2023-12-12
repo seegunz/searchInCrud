366fa5433965421f68f033e4961fcf84e3f53ac4
seegunz <seegn8@gmail.com>
2023-12-12 02:21:17 -0600

    this is a simple search list of a crud operation

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useRef, useState } from 'react'
+
+export default function App() {
+  const input = useRef()
+  const [list, setList] = useState([])
+  const [query, setQuery] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setList([...list, input.current.value])
+    input.current.value = ''
+  }
+
+  const filterList = list.filter(prev=>{
+    return prev.toLowerCase().includes(query.toLocaleLowerCase())
+  })
+
+  console.log(list);
+
+  return (
+    <div>
+      <h1>Search Box</h1>
+      <input type="search" placeholder='search list' value={query} onChange={e=>setQuery(e.target.value)} />
+
+      <br /><br />
+
+      <form onSubmit={handleSubmit}>
+        <input ref={input} />
+        <button type="submit">submit</button>
+      </form>
+
+      <section>{filterList.map((items, i) => <p key={i}>{items}</p>)}</section>
+    </div>
+  )
+}
